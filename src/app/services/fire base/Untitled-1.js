20be99b78dea2ac16a13bb6b17bb599dcfa16661
Mirian-Carrillo <miriancarrillopanzo@gmail.com>
2023-07-07 08:56:35 -0600

    Hola

---
+++ src/app/services/fire base/Untitled-1.js
@@ -0,0 +1,15 @@
+/** @type {import('.next').Nextconfig} */
+const nextConfig ={}
+
+module.exports ={
+    ...nextConfig,
+    env:{ 
+      Zapatitos_api_key:"AIza5yAE3ZYJcy",
+      Zapatitos_auth_domain:"dsm-3a-f6f15.firebaseapp.com",
+      Zapatitos_proyect_id:"dsm-3a-f6f15",
+      Zapatitos_storage_bucket:"dsm-3a-f6f15.appspot.com",
+      Zapatitos_messaging_sender_id: "241306377560",
+      Zapatitos_app_id:"1:241306377560:web:35f4c0406d4794a263d92d"
+
+    }
+  }
---
+++ src/app/services/fire base/index.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "process.env.Zapatitos_api_key",
+  authDomain: "dsm-3a-f6f15.firebaseapp.com",
+  projectId: "dsm-3a-f6f15",
+  storageBucket: "dsm-3a-f6f15.appspot.com",
+  messagingSenderId: "241306377560",
+  appId: "1:241306377560:web:35f4c0406d4794a263d92d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
